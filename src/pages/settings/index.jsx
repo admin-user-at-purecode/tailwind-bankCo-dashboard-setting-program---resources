55e71434d623ae481bd3d3962ee1e7b86f6a0fba
mohammed067 <mohammedrahaman596@gmail.com>
2024-03-10 16:17:48 +0530

    ca_6

--- src/pages/settings/index.jsx
+++ src/pages/settings/index.jsx
@@ -5,8 +5,19 @@ import ProgramAndResources from "../settings/program&resourses";
 function Settings() {
   return (
     <main className="w-full xl:px-[48px] px-6 pb-6 xl:pb-[48px] sm:pt-[156px] pt-[100px]">
-      <div className="grid grid-cols-1 xl:grid-cols-12 bg-white dark:bg-darkblack-600 rounded-xl">
+      <div className="grid grid-cols-6 xl:grid-cols-12 bg-white dark:bg-darkblack-600 rounded-xl">
+        <div className="col-span-6">
         <SettingsSidebar />
+        </div>
+        <div className="col-span-6">
+        <SettingsSidebar />
+        </div>
+        <div className="col-span-6">
+        <SettingsSidebar />
+        </div>
+        <div className="col-span-6">
+        <SettingsSidebar />
+        </div>
         <div className="py-8 px-10 col-span-9 tab-content">
           <ProgramAndResources />
         </div>
